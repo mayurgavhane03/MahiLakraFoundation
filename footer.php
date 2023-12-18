15ff83241406aa50856f5d4c03a1ea7c3ee869cb
KRISHNAA-01 <98485077+KRISHNAA-01@users.noreply.github.com>
2023-12-18 18:38:18 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,161 @@
+ <html>
+  <head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+      footer{
+        margin-top: auto;
+            }
+    </style>
+  </head>
+  <body>
+
+ <!-- Footer -->
+ <footer
+ class="text-center text-lg-start text-dark"
+ style="background-color: #ECEFF1"
+ >
+<!-- Section: Social media -->
+<section
+    class="d-flex justify-content-between p-4 text-white"
+    style="background-color: #2E2E2E"
+    >
+<!-- Left -->
+<div class="me-5">
+<span>Get connected with us on social networks:</span>
+</div>
+<!-- Left -->
+
+<!-- Right -->
+<div>
+<a href="" class="text-white me-4">
+ <i class="fab fa-facebook-f"></i>
+</a>
+<a href="" class="text-white me-4">
+ <i class="fab fa-twitter"></i>
+</a>
+<a href="" class="text-white me-4">
+ <i class="fab fa-google"></i>
+</a>
+<a href="https://www.instagram.com/mahilakrafoundation/?ref=vc.ru" class="text-white me-4">
+ <i class="fab fa-instagram"></i>
+</a>
+<a href="" class="text-white me-4">
+ <i class="fab fa-linkedin"></i>
+</a>
+<a href="https://www.youtube.com/channel/UCBf3RaGO0b6ABb74qVz0xxA" class="text-white me-4">
+ <i class="fab fa-youtube"></i>
+</a> 
+</div>
+<!-- Right -->
+</section>
+<!-- Section: Social media -->
+
+<!-- Section: Links  -->
+<section class="">
+<div class="container text-center text-md-start mt-5">
+<!-- Grid row -->
+<div class="row mt-3">
+ <!-- Grid column -->
+ <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+   <!-- Content -->
+   <h6 class="text-uppercase fw-bold">mahi lakra foundation</h6>
+   <hr
+       class="mb-4 mt-0 d-inline-block mx-auto"
+       style="width: 60px; background-color: #7c4dff; height: 2px"
+       />
+   <p>
+     Here you can use rows and columns to organize your footer
+     content. Lorem ipsum dolor sit amet, consectetur adipisicing
+     elit.
+   </p>
+ </div>
+ <!-- Grid column -->
+
+ <!-- Grid column -->
+ <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+   <!-- Links -->
+   <h6 class="text-uppercase fw-bold">highlights</h6>
+   <hr
+       class="mb-4 mt-0 d-inline-block mx-auto"
+       style="width: 60px; background-color: #7c4dff; height: 2px"
+       />
+   <p>
+     <a href="#!" class="text-dark">Education</a>
+   </p>
+   <p>
+     <a href="#!" class="text-dark">Women Empowerment</a>
+   </p>
+   <p>
+     <a href="#!" class="text-dark">HealthCare</a>
+   </p>
+   <p>
+     <a href="#!" class="text-dark">Food Distribution</a>
+   </p>
+ </div>
+ <!-- Grid column -->
+
+ <!-- Grid column -->
+ <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+   <!-- Links -->
+   <h6 class="text-uppercase fw-bold">Useful links</h6>
+   <hr
+       class="mb-4 mt-0 d-inline-block mx-auto"
+       style="width: 60px; background-color: #7c4dff; height: 2px"
+       />
+   <p>
+     <a href="#!" class="text-dark">home</a>
+   </p>
+   <p>
+     <a href="#!" class="text-dark">about us</a>
+   </p>
+   <p>
+     <a href="#!" class="text-dark">get your certificate</a>
+   </p>
+   <p>
+     <a href="#!" class="text-dark">Help</a>
+   </p>
+ </div>
+ <!-- Grid column -->
+
+ <!-- Grid column -->
+ <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+   <!-- Links -->
+   <h6 class="text-uppercase fw-bold">Contact</h6>
+   <hr
+       class="mb-4 mt-0 d-inline-block mx-auto"
+       style="width: 60px; background-color: #7c4dff; height: 2px"
+       />
+   <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+   <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+   <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+   <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+ </div>
+ <!-- Grid column -->
+</div>
+<!-- Grid row -->
+</div>
+</section>
+<!-- Section: Links  -->
+
+<!-- Copyright -->
+<div
+class="text-center p-3"
+style="background-color: rgba(0, 0, 0, 0.2)"
+>
+© 2023 Copyright:
+<a class="text-dark" href="https://mdbootstrap.com/"
+>Mahilakrafoundation.com</a
+>
+</div>
+<!-- Copyright -->
+</footer>
+<!-- Footer -->
+</div>
+<!-- End of .container -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
---
+++ header.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <title>Document</title>
+  <style>
+    body {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+  background-color: #D4AF37;
+}
+footer{
+  margin-top: auto;
+}
+.navbar {
+  background-color:#2E2E2E !important;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+  height: 60px;
+  padding: 4px;
+  width: 100%;
+}
+
+.navbar-brand {
+  padding: 3px;
+  padding-left: 10px;
+}
+
+.navbar-brand img {
+  background-size: cover;
+  height: 52px;
+  width: 60px;
+}
+
+.navbar-nav a.nav-link {
+  color: #fefefe;
+  font-size: 19px;
+  font-family:Georgia, 'Times New Roman', Times, serif ;
+  padding: 5px;
+}
+
+.navbar-nav .nav-item:hover .nav-link {
+  color: #5b93cb;
+}
+
+.navbar-toggler-icon {
+  background-color: none;
+  border: none;
+  font-size: 18px;
+}
+
+.navbar-toggler {
+  border-color: #007bff;
+  border: none;
+}
+
+.navbar-toggler:hover {
+  background-color: #9fadb4;
+  border: none;
+}
+a.dropdown-item {
+  font-size: 16px;
+  padding: 5px;
+  padding-left: 20px;
+  width: 100%;
+  margin: 0px;
+}
+
+a.dropdown-item:hover {
+  background-color: #063;
+  font-size: 17px;
+  color: #fefefe;
+}
+div#navbarNav.collapse {
+  margin: 10px 15px 10px 10px;
+  position: absolute;
+  margin-left: 90px;
+}
+div.dropdown-menu.show {
+  margin-top: 15px;
+  padding: 3x;
+  border-radius: 2px;
+  box-shadow: 2px 5px 15px rgb(255, 255, 255);
+}
+
+a#servicesDropdown,
+a#contactDropdown {
+  color: rgb(255, 255, 255);
+}
+
+@media (max-width: 992px) {
+  .navbar-brand img {
+    height: 52px; /* Adjust height for smaller screens */
+    width: 60px; /* Allow width to adjust based on height */
+  }
+
+  div.dropdown-menu.show {
+    margin-top: 0px;
+    padding: 0px 3px;
+    border-radius: 2px;
+    border: none;
+    box-shadow: none;
+  }
+
+  div#navbarNav.collapsing {
+    margin: 10px 5px 10px 5px;
+    z-index: 2;
+  }
+  div#navbarNav.collapse.show {
+    margin: 10px 5px 10px 5px;
+    z-index: 2;
+  }
+  div#navbarNav.collapse {
+    margin-left: 10px;
+    position: static;
+  }
+ 
+}
+/* .img-div {
+        position: absolute;
+      } */
+  </style>
+
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand  " href="#">
+      <img src="mahiLakraLogo.png" alt=""/>
+      
+         MAHI LAKRA FOUNDATION
+    
+  </a>
+
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Donate</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="servicesDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Contributions
+          </a>
+          <div class="dropdown-menu" aria-labelledby="servicesDropdown">
+            <a class="dropdown-item" href="#">Education</a>
+            <a class="dropdown-item" href="#">HealthCare</a>
+            <a class="dropdown-item" href="#">Women Empowerment</a>
+            <a class="dropdown-item" href="#">Food Distribution</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="contactDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Contact
+          </a>
+          <div class="dropdown-menu" aria-labelledby="contactDropdown">
+            <a class="dropdown-item" href="#">Email</a>
+            <a class="dropdown-item" href="#">Phone</a>
+            <a class="dropdown-item" href="#">Address</a>
+          </div>
+        </li>
+
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+
+        <li class="nav-item active">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
